2f08ab298b334943eefef6aa7de2cd050c791eaa
Michael Ding <yandy.ding@gmail.com>
2013-09-13 19:44:22 +0800

    确定框架

--- cn/config.rb
+++ cn/config.rb
@@ -53,6 +53,9 @@ activate :sprockets
 #   end
 # end
 
+# Generate sitemap after build
+activate :sitemap_generator
+
 set :css_dir, 'stylesheets'
 
 set :js_dir, 'javascripts'
@@ -89,7 +92,7 @@ configure :build do
   # activate :cache_buster
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Compress PNGs after build
   # First: gem install middleman-smusher
